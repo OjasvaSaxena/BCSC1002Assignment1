f8d766d7bf77a6af8402ae8d0c44d4c535adef61
ojasva <saxenaojasava007@gmail.com>
2020-08-30 22:07:38 +0530

    Complete the book class in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,73 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+ *  User: Ojasva Saxena
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
 package definitions;
-
+import java.util.Objects;
 public class Book {
+    private String bookName;
+    private String authorName;
+    private String ISBNNumber;
+
+    // Parameterized Constructor
+    public Book(String bookName, String authorName, String ISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    // Non-Parameterized Constructor
+    public Book() {
+        this.bookName = null;
+        this.authorName = null;
+        this.ISBNNumber = null;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this. bookName= bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Book Name: " + getBookName() + ", " +
+                "Author Name: " + getAuthorName() + ", " +
+                "13-Digit ISBN Number: " + getISBNNumber() + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getBookName(), book.getBookName()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName()) &&
+                Objects.equals(getISBNNumber(), book.getISBNNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookName(), getAuthorName(), getISBNNumber());
+    }
 }
